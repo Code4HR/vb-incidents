050728fe121776dff57ab1c17cba7e312451a9bb
Kevin Griffin <kevin@kevgriffin.com>
2014-02-22 14:17:21 -0500

    rewrote script to pull data into mongodb based of date range

--- app.js
+++ app.js
@@ -1,40 +1,22 @@
-// run this file with node. entry point for app
-var dates =    require('./lib/dates'),
-     epro =    require('./lib/epro'),
-     cleanup = require('./lib/cleanup'),
-     db =      require('./lib/db');
+/*jslint node: true*/
+var MongoClient = require('mongodb').MongoClient,
+    moment = require('moment'),
+    vbdata = require('./virginiaBeach');
 
-// NOTE (2/20/2014) Much of this still isn't working. 
-// Bret: I'm in the process of merging seperate scripts into one node app
+MongoClient.connect('mongodb://127.0.0.1/test', function (err, db) {
+    "use strict";
+    if (err) {
+        throw err;
+    }
+    console.log("Connected to database");
 
-
-// 1. get an array of all dates from now till date listed below
-// 2. for each array date, query the api for all incidents that day, wait 15s to loop to be a good netcitizen
-// 3. on response, extract out the xml cdata (xpath) and de-encapulate to valid xml
-// 4. DONE: convert xml to json
-// 5. DONE: strip json by dumping json.Incidents.Incident to array
-// 6. DONE: for each item in array, incert document to mongodb
-
-// this is how far we want to go back from today
-var firstDate = new Date(2013,2,2);
-console.log("create firstDay " + firstDate.toDateString());
-
-//var dateArray = dates.allDays(firstDate);
-//console.log(dateArray);
-//
-//var i = 0;
-//  console.log('got all dates');
-//  // for (var i = 0; i < dateArray.length; i++) {
-//  do {
-//    console.log('date: ' + dateArray[i]);
-//     epro.incidentsForDate(dateArray[i], function(err, result) {
-//       console.log('got epo results for ' + result);
-//       cleanup.soap2json(result, function(err, result) {
-//         console.log('cleaned up data for ' + result);
-//         db.insertAll(result, function(err, result) {
-//           console.log(result);
-//         });
-//       });
-//     });
-//    i++;
-//  } while(i < dateArray.length);
+    var collection = db.collection('incidents');
+    vbdata({
+        collection: collection,
+        startDate: moment().subtract('months', 12).format("YYYY-MM-DD"),
+        endDate: moment().subtract('days', 1).format('YYYY-MM-DD')
+    }).done(function () {
+        console.log("All done!");
+        db.close();
+    });
+});
---
+++ virginiaBeach/index.js
@@ -0,0 +1,91 @@
+/*jslint node: true*/
+/*jslint nomen: true*/
+
+var moment = require('moment'),
+    q = require('q'),
+    soap = require('soap'),
+    url = 'https://public.vbgov.com/Secure/service.asmx?wsdl',
+    parseString = require('xml2js').parseString,
+    _ = require('lodash');
+
+function convertArrayToValue(obj) {
+    "use strict";
+    _.each(obj, function (prop, propName) {
+        if (_.isArray(prop)) {
+            if (prop.length === 1) {
+                obj[propName] = prop[0];
+            }
+            convertArrayToValue(obj[propName]);
+        }
+    });
+}
+
+function processDate(dateToProcess, collection) {
+    "use strict";
+    var defer = q.defer();
+
+    console.log("start " + dateToProcess);
+
+    soap.createClient(url, function (err, client) {
+        client.addSoapHeader("<UserCredentials xmlns=\"http://public.vbgov.com/secure\"><Email>bret@codeforamerica.org</Email><UserName>bret@codeforamerica.org</UserName><Password>krXeARx8C#3ZnMc</Password></UserCredentials>");
+        client.GetIncidentData({
+            forDate: dateToProcess
+        }, function (err, result) {
+            if (err) {
+                console.log("error - timeout?");
+                defer.reject(err);
+                return;
+            }
+            parseString(result.GetIncidentDataResult, function (err2, result2) {
+                var realIncidents = result2.Incidents.Incident;
+                _.each(realIncidents, function (val) {
+                    convertArrayToValue(val);
+                });
+
+                console.log("got data for " + dateToProcess);
+
+                collection.insert(realIncidents, function (err, docs) {
+                    console.log("end " + dateToProcess);
+                    defer.resolve();
+                });
+            });
+        });
+    });
+    return defer.promise;
+}
+
+module.exports = function (options) {
+    "use strict";
+    var currentSearchDate,
+        momentEndDate,
+        allRequests = [],
+        defer = q.defer();
+
+    if (options.collection === undefined) {
+        console.log("collection not found.");
+        return;
+    }
+    if (options.startDate === undefined) {
+        options.startDate = moment().subtract('month', 1).format("YYYY-MM-DD");
+        console.log("No start date provided.  Defaulting to " + options.startDate);
+    }
+    if (options.endDate === undefined) {
+        options.endDate = moment().subtract('days', 1).format("YYYY-MM-DD");
+        console.log("No end date provided.  Defaulting to " + options.endDate);
+    }
+
+    currentSearchDate = moment(options.startDate);
+    momentEndDate = moment(options.endDate);
+
+    while (currentSearchDate.isBefore(momentEndDate) || currentSearchDate.isSame(momentEndDate)) {
+        console.log("Adding " + currentSearchDate.format("YYYY-MM-DD") + " to the request pool");
+        allRequests.push(processDate(currentSearchDate.format("YYYY-MM-DD"), options.collection));
+        currentSearchDate = currentSearchDate.add('days', 1);
+    }
+
+    q.allSettled(allRequests).done(function () {
+        defer.resolve();
+    });
+
+    return defer.promise;
+};
