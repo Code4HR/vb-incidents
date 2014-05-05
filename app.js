c32a5afd0a7264fb0d91af11b6293e29852bf7f5
John Hadzima <jhadz81@gmail.com>
2014-05-05 17:32:15 -0400

    continued implementation of web app
    - count of incidents and date range on footer
    - added neighborhood search for incidents with autocomplete
    - updated jade views
    - zip code summary page with data and pie chart

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-/*jslint node: true*/
-var express = require('express');
+﻿var express = require('express');
 var routes = require('./routes');
+var lib = require('./lib/lib');
 var http = require('http');
 var path = require('path');
 var stylus = require('stylus');
 var nib = require('nib');
-
+var moment = require('moment');
 // db setup
 var mongo = require('mongodb');
 var mongo = require('mongoskin');
@@ -21,37 +21,56 @@ function compile(str, path) {
 		.use(nib());
 }
 
-// SET
+// all environments
 app.set('port', process.env.PORT || 1337);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('env', 'development');
-
-// USE
-app.use(express.json());
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
 app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' === app.get('env')) {
-	app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
+
+// get total # of incidents, first and last incident and save to app variables
+db.collection('incidents').find().count(function (err, count) {
+	"use strict";
+	app.locals.totalIncidents = lib.numberWithCommas(count);
+});
+db.collection('incidents').find({}, { sort: [['ReportDate', 'desc']] }).limit(1).toArray(function (err, result) {
+	"use strict";
+	if (result !== null) {
+		app.locals.firstIncidentDate = moment(result[0].ReportDate).format("MM/DD/YYYY h:mm a");
+	}
+});
+db.collection('incidents').find({}, { sort: 'ReportDate' }).limit(1).toArray(function (err, result) {
+	"use strict";
+	if (result !== null) {
+		app.locals.lastIncidentDate = moment(result[0].ReportDate).format("MM/DD/YYYY h:mm a");
+	}
+});
 
 // GET 
 app.get("/", routes.index);
 app.get("/trends", routes.trends);
 app.get("/about", routes.about);
+app.get("/neighborhood", routes.neighborhood(db));
 app.get("/incidents", routes.incidents(db));
 app.get("/cities", routes.getCityList(db));
 app.get("/zipcodes", routes.getZipCodes(db));
 app.get("/neighborhoods", routes.getNeighborhoods(db));
 app.get("/crimes", routes.getCrimes(db));
-app.get("/zipgrouping", routes.zipCodeGroup);
+app.get("/incidents_by_zip_code", routes.zipCodeGroup);
+app.get("/search_neighborhood", routes.search_neighborhood);
 
 http.createServer(app).listen(app.get('port'), function () {
-	"use strict";
-	console.log('Express server listening on port ' + app.get('port'));
+    "use strict";
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,102 +1,166 @@
-var MongoClient = require('mongodb').MongoClient;
+/*jslint node:true*/
+"use strict";
 
-function numberWithCommas(x) {
+var MongoClient = require( 'mongodb' ).MongoClient;
+
+exports.index = function ( req, res ) {
 	"use strict";
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-exports.index = function (req, res) {
-    "use strict";
-	res.render('index', { title: 'Virginia Beach Crime Data' });
+	res.render( 'index' );
 };
 
-exports.trends = function (req, res) {
-    "use strict";
-	res.render('trends', { title: 'Virginia Beach Crime Data Trends' });
+exports.trends = function ( req, res ) {
+	"use strict";
+	res.render( 'trends' );
 };
 
-exports.about = function (req, res) {
-    "use strict";
-	res.render('about', { title: 'About Virginia Beach Crime Data' });
+exports.about = function ( req, res ) {
+	"use strict";
+	res.render( 'about' );
 };
 
-exports.incidents = function (db) {
-    "use strict";
-    var moment = require('moment'),
-		totalIncidents;
+exports.neighborhood = function ( db ) {
+	"use strict";
 
-	// do a find and get the cursor count
-	db.collection('incidents').find().count(function (err, count) {
-		totalIncidents = numberWithCommas(count);
-		db.close();
+	var moment = require( 'moment' );
+
+	// get the incidents for the neighborhood
+	return function ( req, res ) {
+		db.collection( 'incidents' ).find( { 'Location.Neighborhood': req.query.neighborhood }, { sort: [['ReportDate', 'desc']] }).limit( 5 ).toArray( function ( err, result ) {
+			if (err) {
+				res.send(err);
+				return;
+			}
+			res.render( 'neighborhood', { neighborhood: req.query.neighborhood, "incidents": result, moment: moment });
+		});
+	}
+};
+
+exports.search_neighborhood = function ( req, res ) {
+	"use strict";
+
+	MongoClient.connect( 'mongodb://127.0.0.1/incidentreports', function ( err, db ) {
+		if ( err ) {
+			res.send( JSON.stringify( err ), { 'Content-Type': 'application/json' }, 404 );
+			return;
+		}
+
+		// get the search term
+		var regex = new RegExp( req.query["term"], 'i' );
+
+		var collection = db.collection( 'incidents' );
+		collection.find( { 'Location.Neighborhood': regex }, { sort: [['Location.Neighborhood', 'desc']] }).limit( 20 ).toArray( function ( err, result ) {
+			if ( err ) {
+				res.send( JSON.stringify( err ), { 'Content-Type': 'application/json' }, 404 );
+				return;
+			}
+
+			res.send( result, { 'Content-Type': 'application/json' }, 200 );
+		});
 	});
-
-    return function (req, res) {
-		db.collection('incidents').find({}, { sort : [['ReportDate', 'desc']] }).limit(50).toArray(function (err, result) {
-            res.render('incidents', { "incidents" : result, moment: moment, totalIncidents : totalIncidents });
-		});
-    };
 };
 
-exports.getCityList = function (db) {
-    "use strict";
-    return function (req, res) {
-		db.collection('incidents').distinct("Location.City", function (e, result) {
-			res.send(result.sort());
-		});
-    };
-};
-
-exports.getZipCodes = function (db) {
-    "use strict";
-    return function (req, res) {
-		db.collection('incidents').distinct("Location.Zip", function (e, result) {
-			res.send(result.sort());
-		});
-    };
-};
-
-exports.getNeighborhoods = function (db) {
-    "use strict";
-    return function (req, res) {
-		db.collection('incidents').distinct("Location.Neighborhood", function (e, result) {
-			res.send(result.sort());
-		});
-    };
-};
-
-exports.getCrimes = function (db) {
-    "use strict";
-    return function (req, res) {
-		db.collection('incidents').distinct("Crimes.Crime.CrimeDescription", function (e, result) {
-			res.send(result.sort());
-		});
-    };
-};
-
-exports.zipCodeGroup = function (req, res) {
+exports.incidents = function ( db ) {
 	"use strict";
-    MongoClient.connect('mongodb://127.0.0.1/incidentreports', function (err, db) {
-        if (err) {
-            res.send(500);
-            return;
-        }
+	var moment = require( 'moment' );
 
-		var collection = db.collection('incidents');
-		collection.aggregate([
+	return function ( req, res ) {
+		db.collection( 'incidents' ).find( {}, { sort: [['ReportDate', 'desc']] }).limit( 50 ).toArray( function ( err, result ) {
+			res.render( 'incidents', {
+				"incidents": result,
+				moment: moment
+			});
+		});
+	};
+};
+
+exports.getCityList = function ( db ) {
+	"use strict";
+	return function ( req, res ) {
+		db.collection( 'incidents' ).distinct( "Location.City", function ( e, result ) {
+			res.send( result.sort() );
+		});
+	};
+};
+
+exports.getZipCodes = function ( db ) {
+	"use strict";
+	return function ( req, res ) {
+		db.collection( 'incidents' ).distinct( "Location.Zip", function ( e, result ) {
+			res.send( result.sort() );
+		});
+	};
+};
+
+exports.getNeighborhoods = function ( db ) {
+	"use strict";
+	return function ( req, res ) {
+		db.collection( 'incidents' ).distinct( "Location.Neighborhood", function ( e, result ) {
+			res.send( result.sort() );
+		});
+	};
+};
+
+exports.getCrimes = function ( db ) {
+	"use strict";
+	return function ( req, res ) {
+		db.collection( 'incidents' ).distinct( "Crimes.Crime.CrimeDescription", function ( e, result ) {
+			res.send( result.sort() );
+		});
+	};
+};
+
+exports.zipCodeGroup = function ( req, res ) {
+	"use strict";
+	MongoClient.connect( 'mongodb://127.0.0.1/incidentreports', function ( err, db ) {
+		if ( err ) {
+			res.send( 500 );
+			return;
+		}
+		var collection = db.collection( 'incidents' ),
+			_ = require( 'lodash' ),
+			lib = require('../lib/lib');
+		var totalIncidents,
+			VBZipCodes = ['23451', '23452', '23453', '23454', '23455', '23456', '23457', '23458', '23459', '23460', '23461', '23462', '23463', '23464', '23465', '23466', '23467', '23471', '23479'],
+			zipData = [],
+			zipCount = 0,
+			chartData;
+
+		// do a find and get the cursor count
+		collection.find().count( function ( err, count ) {
+			totalIncidents = count;
+		});
+
+		// group and count by zip
+		collection.aggregate( [
 			{
 				$group: {
 					_id: "$Location.Zip",
 					count: { $sum: 1 }
 				}
 			},
-			{ $sort: {_id: 1} },
+			{ $sort: { _id: 1 } },
 			{ $match: { count: { $gt: 1 } } }
-		],  function (lookupErr, lookupData) {
-            //res.send(lookupData.sort());
-			res.render('zipCodeGroup', { "incidents" : lookupData });
-            db.close();
-            return;
-        });
-    });
+		], function ( lookupErr, lookupData ) {
+				chartData = "[['Zip Code', 'Incidents']";
+
+				// combine non VB zip codes into Other
+				for ( var key in lookupData ) {
+					if ( _.contains(VBZipCodes, lookupData[key]._id)) {
+						zipData.push( { '_id': lookupData[key]._id, 'count': lib.numberWithCommas( lookupData[key].count ), 'percentofTotal': ( ( lookupData[key].count / totalIncidents ) * 100 ).toFixed( 2 ), 'zipURL': 'https://www.google.com/maps/place/Virginia+Beach,+VA+' + lookupData[key]._id, 'zipURLTarget': '_blank' });
+						chartData = chartData + ",['" + lookupData[key]._id + "', " + lookupData[key].count + "]";
+					}
+					else {
+						zipCount = zipCount + lookupData[key].count;
+					}
+				}
+				chartData = chartData + ",['Other', " + zipCount + "]]";
+				zipData.push( { '_id': '*Other', 'count': lib.numberWithCommas( zipCount ), 'percentofTotal': ( ( zipCount / totalIncidents ) * 100 ).toFixed( 2 ), 'zipURL': '#', 'zipURLTarget': '_self' });
+				zipData.push( { '_id': 'Total Incidents', 'count': req.app.get( 'totalIncidents' ), 'percentofTotal': '100', 'zipURL': '#', 'zipURLTarget': '_self' });
+
+				res.render( 'zipCodeGroup', { "incidents": zipData, "chartData" : chartData});
+
+				db.close();
+				return;
+			});
+	});
 };
